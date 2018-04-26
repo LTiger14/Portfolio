03f0a39e23327b0bdd28f8d4c54a4c27d47043d0
Louis Pierrestiger <plasma93@hotmail.com>
2018-04-25 20:43:12 -0400

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { ParticlesModule } from 'angular-particle';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParticlesModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
